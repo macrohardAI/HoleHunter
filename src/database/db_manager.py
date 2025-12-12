94a9f1a86afec55011c2bf53c11eb1de5271d4a6
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-12 23:46:08 +0800

    make logic functions that can conncet to db

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -2,11 +2,24 @@
 Storing/retrieving hole detections.
 """
 import sqlite3
-
+from pathlib import Path
 
 class DatabaseManager:
-    def __init__(self):
-        pass
+    def __init__(self, db_path: str = 'database/hole.db', schema_path: str = 'src/database/schema.sql'):
+        self.db_path = db_path
+        self.schema_path = schema_path
+        try:
+            self.db_path.parent.mkdir(parents = True, exist_ok = True)
+        except Exception as e:
+            print(f"ERROR : Gagal membuat direktori database : {e}")
+            sys.exit(1)
+
+    def connect(self):
+        try:
+            return sqlite3.connect(str(self.db_path))
+        except sqlite3.Error as e:
+            print(f"ERROR : Gagal terhubung ke database : {e}")
+            return None
 
     def create_tables(self):
         """Initialize database schema"""
