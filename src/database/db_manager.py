6195502f682046f3404ef28249444523685647ef
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-12 23:58:34 +0800

    finishing create_tables function (i'm so tired)

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -3,11 +3,12 @@ Storing/retrieving hole detections.
 """
 import sqlite3
 from pathlib import Path
+import sys
 
 class DatabaseManager:
     def __init__(self, db_path: str = 'database/hole.db', schema_path: str = 'src/database/schema.sql'):
-        self.db_path = db_path
-        self.schema_path = schema_path
+        self.db_path = Path(db_path)
+        self.schema_path = Path(schema_path)
         try:
             self.db_path.parent.mkdir(parents = True, exist_ok = True)
         except Exception as e:
@@ -18,12 +19,34 @@ class DatabaseManager:
         try:
             return sqlite3.connect(str(self.db_path))
         except sqlite3.Error as e:
-            print(f"ERROR : Gagal terhubung ke database : {e}")
+            print(f"ERROR : Gagal terhubung ke database {self.db_path} : {e}")
             return None
 
     def create_tables(self):
         """Initialize database schema"""
-        raise NotImplementedError
+        conn = self.connect()
+        if conn is None:
+            return
+        
+        try:
+            if not self.schema_path.exists():
+                print(f"ERROR : File tidak ditemukan : {self.schema_path}")
+                return
+            #read all sql scripts
+            sql_script = self.schema_path.read_text()
+
+            cursor = conn.cursor()
+            cursor.executescript(sql_script)
+            conn.commit()
+            print(f"Database schema dibuat/di update di : {self.db_path} ")
+
+        except sqlite3.Error as e:
+            print(f"ERROR: Gagal mengeksekusi skema SQL. Pesan: {e}")
+        except Exception as e:
+            print(f"ERROR: Terjadi kesalahan saat membuat tabel: {e}")
+        finally:
+            conn.close()
+
 
     def insert_detection(self, image_path: str, confidence: float, gps: dict) -> int:
         """Store detection"""
