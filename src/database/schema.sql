b202b973494acea81407ffcf7f0bb630dbf27c5b
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-12 20:54:37 +0800

    add indeks so that user can search the data easier

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -4,7 +4,15 @@ CREATE TABLE IF NOT EXISTS holes (
     time_detection TEXT NOT NULL,
     latitude REAL NOT NULL,
     longitude REAL NOT NULL,
-    prediction_label TEXT NOT NULL,
+    prediction_label TEXT NOT NULL,   --(hole or not hole, small, large)
     confidence REAL TEXT NOT NULL,    --(0-1)
     valildate_human TEXT            --(yes, no)
 )
+
+--indeks utk cari lokasi hole faster
+CREATE INDEX IF NOT EXISTS location
+ON detections(latitude,longitude);
+
+--indeks utk cari label
+CREATE INDEX IF NOT EXISTS label
+ON detections(prediction_label);
