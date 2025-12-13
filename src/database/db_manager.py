5c7044aa7725bdc0d4bdcb586dd8cbd3baaa1c8b
Sulthan Farizan Fawwaz <farizanfawwaz54@gmail.com>
2025-12-13 10:54:57 +0800

    one logic function again

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -7,6 +7,7 @@ import sys
 
 class DatabaseManager:
     def __init__(self, db_path: str = 'database/hole.db', schema_path: str = 'src/database/schema.sql'):
+        """Initialize db manajer needs"""
         self.db_path = Path(db_path)
         self.schema_path = Path(schema_path)
         try:
@@ -39,7 +40,6 @@ class DatabaseManager:
             cursor.executescript(sql_script)
             conn.commit()
             print(f"Database schema dibuat/di update di : {self.db_path} ")
-
         except sqlite3.Error as e:
             print(f"ERROR: Gagal mengeksekusi skema SQL. Pesan: {e}")
         except Exception as e:
@@ -48,13 +48,51 @@ class DatabaseManager:
             conn.close()
 
 
-    def insert_detection(self, image_path: str, confidence: float, gps: dict) -> int:
-        """Store detection"""
-        raise NotImplementedError
+    def insert_detection(self, image_filename: str, time_detection: str, latitude: float, longitude: float, prediction_label: str, confidence: float, is_validate: str) -> int:
+        """Store hole detection and return -1 if it fails"""
+        conn = self.connect()
+        if conn is None:
+            return -1
+        
+        query ="""
+        INSERT INTO holes (image_filename, time_detection, latitude, longitude, prediction_label, confidence, is_validate)
+        VALUES(?, ?, ?, ?, ?, ?, ?)
+        """
+
+        try:
+            cursor = conn.cursor()
+            cursor.execute(query, (image_filename, time_detection, latitude, longitude, prediction_label, confidence, is_validate))
+            conn.commit()
+            return cursor.lastrowid  #Get the new data ID that stored
+        except sqlite3.IntegrityError as e:
+            """Special error if there are restrictions that have been violated"""
+            print(f"ERROR (Integrity): Gagal menyisipkan data. Cek batasan skema: {e}")
+            return -1
+        except sqlite3.Error as e:
+            print(f"ERROR (SQL): Gagal menyisipkan data: {e}")
+            return -1
+        finally:
+            conn.close()
 
     def get_all_detections(self) -> list:
-        """Retrieve all detections"""
-        raise NotImplementedError
+        """Retrieve all detections fromm table HOLES"""
+        conn = self.connect()
+        if conn is None:
+            return []
+        
+        query = "SELECT * FROM holes"
+        try : 
+            """Use row factory so that it can be more easily to read"""
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            cursor.execute(query)
+            results = [dict(row) for row in cursor.fetchall()]  #change query result to list of dicts
+            return results
+        except sqlite3.Error as e:
+            print(f"ERROR : Gagal mengambil data dari database : {e}")
+            return []
+        finally:
+            conn.close()
 
     def get_detections_by_location(self, location: dict, radius: float) -> list:
         """Retrieve detections near location"""
